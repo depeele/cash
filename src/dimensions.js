fa79fe62e8f8f3648c3c1e6824227c45d28d59dc
D. Elmo Peele <Elmo.Peele@gmail.com>
2017-09-30 16:16:01 -0400

    Handle [inner/outer]width/height() for the top-level window.

--- src/dimensions.js
+++ src/dimensions.js
@@ -7,18 +7,31 @@ cash.each(['Width','Height'], prop => {
 
   const lower = prop.toLowerCase();
 
-  cash.fn[lower] = function(){ return this[0].getBoundingClientRect()[lower]; };
+  cash.fn[lower] = function(){
+    const el  = this[0];
+    return (cash.isWindow(el)
+              ? el.document.documentElement['client'+ prop]
+              : el.getBoundingClientRect()[lower]);
+  };
 
-  cash.fn['inner' + prop] = function(){ return this[0]['client' + prop]; };
+  cash.fn['inner' + prop] = function(){
+    const el  = this[0];
+    return (cash.isWindow(el)
+              ? el.document.documentElement['client'+ prop]
+              : el['client' + prop]);
+  };
 
   cash.fn['outer' + prop] = function(margins) {
-    return this[0]['offset' + prop] +
-            ( margins
-                ?  _compute(this, 'margin' + ( prop === 'Width'
-                                                ? 'Left' : 'Top' )) +
-                   _compute(this, 'margin' + ( prop === 'Width'
-                                                ? 'Right' : 'Bottom' ))
-                : 0 );
+    const el  = this[0];
+    return (cash.isWindow(el)
+              ? el['inner'+ prop]
+              : el['offset' + prop] +
+                ( margins
+                  ?  _compute(this, 'margin' + ( prop === 'Width'
+                                                  ? 'Left' : 'Top' )) +
+                     _compute(this, 'margin' + ( prop === 'Width'
+                                                  ? 'Right' : 'Bottom' ))
+                  : 0 ));
   };
 
 });
