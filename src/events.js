27707bf520ac897617cb77951a5c0b97ab352a55
D. Elmo Peele <Elmo.Peele@gmail.com>
2017-10-01 15:36:39 -0400

    Add a writable, pseudo-event and add focus/blur().

--- src/events.js
+++ src/events.js
@@ -1,3 +1,4 @@
+/* jshint laxbreak: true, eqnull:true */
 function _registerEvent(node, eventName, callback) {
   const eventCache  = cash.getData(node,'_cashEvents') ||
                       cash.setData(node, '_cashEvents', {});
@@ -26,6 +27,66 @@ function _removeEvent(node, eventName, callback) {
   }
 }
 
+function _returnTrue()  { return true; }
+function _returnFalse() { return false; }
+
+function _makeGetSetProperty( obj, target, key ) {
+  Object.defineProperty( obj, key, {
+    get()     { return target[key]; },
+    set(val)  { target[key] = val; },
+  });
+}
+
+cash.Event = function( src, props ) {
+  // Allow instantiation without 'new'
+  if ( !(this instanceof cash.Event) ) {
+    return new cash.Event( src, props );
+  }
+
+  const srcEvent  = (src && src.type && src instanceof Event
+                      ? src
+                      : new CustomEvent( src ));
+
+  // Mirror all properties of the source event
+  for (let key in srcEvent) {
+    if (props && key in props ||
+        (key === 'target' || key === 'currentTarget')) {
+      // Properties that need to be modifiable (and thus not directly mirrored)
+      this[key] = srcEvent[key];
+
+    } else {
+      // Actually reference the source event
+      _makeGetSetProperty( this, srcEvent, key );
+    }
+  }
+
+  this.originalEvent = srcEvent;
+
+  // The target should NOT be a text node
+  if (srcEvent.target && srcEvent.target.nodeType === 3) {
+    this.target = srcEvent.target.parentNode;
+  }
+
+  // Include explicitly provided properties
+  if ( props ) {
+    cash.extend( this, props );
+  }
+
+  // Include a jQuery-like isDefaultPrevented()'
+  this.isDefaultPrevented = (srcEvent.defaultPrevented ||
+                             (srcEvent.defaultPrevented === undefined &&
+                              srcEvent.returnValue === false)
+                              ? _returnTrue
+                              : _returnFalse);
+
+  // Create a timestamp if the incoming event doesn't have one
+  if (this.timeStamp == null) { this.timeStamp = Date.now(); }
+
+
+  // Mark this event as "fixed"
+  this[ cash.uid ] = true;
+};
+
 cash.fn.extend({
 
   off(eventName, callback) {
@@ -51,7 +112,11 @@ cash.fn.extend({
       return this;
     }
 
-    if ( delegate ) {
+    if ( !cash.isFunction(callback) && delegate === false ) {
+      // Handle on('event', false) to squelch an event
+      callback = _returnFalse;
+
+    } else if ( delegate ) {
       const originalCallback  = callback;
       callback = function( evt ) {
         let target = evt.target;
@@ -64,7 +129,13 @@ cash.fn.extend({
         }
 
         if (target) {
-          originalCallback.call(target, evt);
+          // Make a writable Event adjusted for delegation
+          const newEvent = cash.Event( evt, {
+            delegateTarget : evt.currentTarget,
+            currentTarget  : target,
+          });
+
+          originalCallback.call(target, newEvent);
         }
       };
     }
@@ -92,6 +163,16 @@ cash.fn.extend({
     evt.initEvent(eventName, true, false);
 
     return this.each(el => el.dispatchEvent(evt));
+  },
+
+  focus() {
+    //return this;
+    return this.each( el => el.focus() );
+  },
+
+  blur() {
+    //return this;
+    return this.each( el => el.blur() );
   }
 
 });
