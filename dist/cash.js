c2a5a4a7cd125d52939b7c1d5e3a1b11938fe6e7
D. Elmo Peele <Elmo.Peele@gmail.com>
2017-10-02 05:52:21 -0400

    Re-build

--- dist/cash.js
+++ dist/cash.js
@@ -62,6 +62,18 @@
     return _frag.body.childNodes;
   }
   
+  
+  function _contains( container, target ) {
+    const containerEl   = (container.nodeType === 9
+                            ? container.documentElement
+                            : container);
+    const targetParent  = (target && target.parentNode);
+  
+    return (container === targetParent ||
+            !!( targetParent && targetParent.nodeType === 1 &&
+                containerEl.contains( targetParent ) ) );
+  }
+  
   function _onReady(fn) {
     if ( doc.readyState !== 'loading' ) { fn(); }
     else { doc.addEventListener('DOMContentLoaded', fn); }
@@ -153,6 +165,7 @@
     isString    : { value: _isString },
     find        : { value: _find },
     proxy       : { value: _proxy },
+    contains    : { value: _contains },
   });
   
   Init.prototype = cash.prototype;
@@ -1031,11 +1044,75 @@
     text(content) {
       if ( content === undefined ) { return this[0].textContent; }
       return this.each(el => el.textContent = content);
-    }
+    },
   
+    detach() {
+      return this.remove();
+    },
   });
   
+  /* jshint laxbreak:true, eqnull:true, maxstatements:31 */
   const docEl = doc.documentElement;
+  
+  cash.offset = {
+    setOffset( elem, options, index ) {
+      const curElem   = cash( elem );
+      const position  = curElem.css('position');
+      const props     = {};
+      let   curPosition;
+      let   curLeft;
+      let   curCSSTop;
+      let   curTop;
+      let   curOffset;
+      let   curCSSLeft;
+      let   calculatePosition;
+  
+      // Set position first, in-case top/left are set even on static elem
+      if ( position === 'static' ) {
+        elem.style.position = 'relative';
+      }
+  
+      curOffset         = curElem.offset();
+      curCSSTop         = curElem.css( 'top' );
+      curCSSLeft        = curElem.css( 'left' );
+      calculatePosition = ( position === 'absolute' || position === 'fixed' ) &&
+                          ( curCSSTop + curCSSLeft ).indexOf( 'auto' ) > -1;
+  
+      // Need to be able to calculate position if either
+      // top or left is auto and position is either absolute or fixed
+      if ( calculatePosition ) {
+        curPosition = curElem.position();
+        curTop      = curPosition.top;
+        curLeft     = curPosition.left;
+  
+      } else {
+        curTop  = parseFloat( curCSSTop ) || 0;
+        curLeft = parseFloat( curCSSLeft ) || 0;
+      }
+  
+      if ( cash.isFunction( options ) ) {
+  
+        /* Use cash.extend here to allow modification of coordinates argument
+         * (gh-1848)
+         */
+        options = options.call( elem, index, cash.extend( {}, curOffset ) );
+      }
+  
+      if ( options.top != null ) {
+        props.top = ( options.top - curOffset.top ) + curTop;
+      }
+      if ( options.left != null ) {
+        props.left = ( options.left - curOffset.left ) + curLeft;
+      }
+  
+      if ( 'using' in options ) {
+        options.using.call( elem, props );
+  
+      } else {
+        curElem.css( props );
+      }
+    }
+  };
   
   cash.fn.extend({
   
@@ -1047,7 +1124,16 @@
       };
     },
   
-    offset(){
+    offset( options ){
+      if ( arguments.length ) {
+        // Preserve chaining for setters
+        return (options === undefined
+                  ? this  // :XXX: Should this be 'this.offset()' ???
+                  : this.each( function(idex) {
+                      cash.offset.setOffset( this, options, idex );
+                    }) );
+      }
+  
       const rect = this[0].getBoundingClientRect();
       return {
         top : rect.top  + win.pageYOffset - docEl.clientTop,
@@ -1058,6 +1144,7 @@
     offsetParent(){ return cash(this[0].offsetParent); }
   
   });
+  
   
   /* jshint laxbreak: true */
   cash.fn.extend({
